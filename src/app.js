989b5daabfd5641630b24c37ce0685a9f87521e3
abhinash-07 <a8239517@gmail.com>
2024-09-13 07:18:02 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
+
+app.use(express.json({
+    limit: "16kb"
+}))
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+
+
+const app = express()
+
+export {app}
