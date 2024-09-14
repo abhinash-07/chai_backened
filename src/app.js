0986f36975bb00bc6322874b440295221da6897f
abhinash-07 <a8239517@gmail.com>
2024-09-14 08:08:08 +0530

    multer cloudinary

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
-
+const app = express()
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true
@@ -17,6 +17,6 @@ app.use(cookieParser())
 
 
 
-const app = express()
+
 
 export {app}
