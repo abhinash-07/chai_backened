d19ac2b8311b80ec0813e666e1be6f547a46858f
abhinash-07 <a8239517@gmail.com>
2024-09-18 06:57:17 +0530

    wednesday

--- src/app.js
+++ src/app.js
@@ -14,9 +14,10 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+import userRouter from './routes/user.routes.js'
 
-
-
+//router declaration 
+app.use("/api/v1/users", userRouter)
 
 
 export {app}
